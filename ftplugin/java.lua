9a05efa62a5d9dd9e33fa503c4abfafffdd7289b
michael.goldsmith <michael.goldsmith@datalex.com>
2025-07-07 08:46:10 +0700

    java config

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,5 +1,19 @@
 local config = {
     cmd = {'C:\\Dev\\Workspace\\eclipse.jdt.ls\\org.eclipse.jdt.ls.product\\target\\repository\\bin\\jdtls.bat'},
     root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
+    settings = {
+	java = {
+	    configuration = {
+	    
+		runtimes = {
+		    {
+			name = "JDK8",
+			path = "C:\\Dev\\Apps\\java\\jdk8u452-9"
+		    }
+
+		}
+	    }
+	}
+    }
 }
 require('jdtls').start_or_attach(config)
